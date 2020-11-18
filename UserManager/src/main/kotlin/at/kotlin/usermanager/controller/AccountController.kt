7d2a747e99fc724c11b87a59d91d05c65ec29457
Johannes Schumeth <johannes.schumeth@gmail.com>
2020-11-18 18:09:15 +0100

    changes from code with me session

--- UserManager/src/main/kotlin/at/kotlin/usermanager/controller/AccountController.kt
+++ UserManager/src/main/kotlin/at/kotlin/usermanager/controller/AccountController.kt
@@ -2,8 +2,10 @@ package at.kotlin.usermanager.controller
 
 import at.kotlin.usermanager.entities.Account
 import at.kotlin.usermanager.services.AccountService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/account")
@@ -22,5 +24,15 @@ class AccountController(
     fun findAll(): ResponseEntity<List<Account>> {
         val accounts = accountService.findAll()
         return ResponseEntity.ok().body(accounts)
+    }
+
+    @PostMapping("/login")
+    fun login(
+            @RequestHeader("username") username: String,
+            @RequestHeader("password") password: String
+    ) : ResponseEntity<*> {
+        if(!accountService.login(Account(null, username, password))) {
+            throw ResponseStatusException(HttpStatus.FORBIDDEN, "username oder password nicht korrekt")
+        }
     }
 }
---
+++ UserManager/src/main/kotlin/at/kotlin/usermanager/dtos/LoginObject.kt
@@ -0,0 +1,6 @@
+package at.kotlin.usermanager.dtos
+
+data class LoginObject (
+        val username: String,
+        val password: String
+)
