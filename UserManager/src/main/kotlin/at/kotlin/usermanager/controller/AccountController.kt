0b2d0ec57862bac75555814a704f30e520e6daa0
JoeNeu <37147189+JoeNeu@users.noreply.github.com>
2020-11-23 18:05:56 +0100

    fix refactor

--- UserManager/src/main/kotlin/at/kotlin/usermanager/controller/AccountController.kt
+++ UserManager/src/main/kotlin/at/kotlin/usermanager/controller/AccountController.kt
@@ -8,7 +8,6 @@ import at.kotlin.usermanager.exceptions.BannedAccountException
 import at.kotlin.usermanager.exceptions.InvalidLoginCredentialsException
 import at.kotlin.usermanager.exceptions.UsernameAlreadyExistsException
 import at.kotlin.usermanager.services.AccountService
-import at.kotlin.usermanager.services.JwtTokenGenerator
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
